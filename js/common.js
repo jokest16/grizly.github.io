96b1d5a13547026235446afd468364ea91470784
jokest16 <jokest16@gmail.com>
2016-09-21 23:38:49 +0300

    commit

---
+++ js/common.js
@@ -0,0 +1,34 @@
+
+$(function() {
+
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
+	$("a[href=#callback]").click(function(){
+		$("#callback .forname").val($(this).data("form"));
+	});
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
